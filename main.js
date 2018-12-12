ded914ffe843d17d00f0ee6ea9d1f5fee5033539
jimipulsar <jimipulsar@gmail.com>
2018-12-12 11:04:46 +0100

    Esercizio corretto e modificato

--- main.js
+++ main.js
@@ -1,29 +1,45 @@
 //Creare una griglia formata da 8x8 quadratini tutti bianchi. 15 di questi quadratini, scelti a caso all'inizio, se cliccati diventano rossi, gli altri diventano verdi. Sopra alla griglia deve esserci un contatore che conta quanti sono rossi e quanti verdi sono stati scoperti.
 
-var n = 0;
-$(".quadrato1").one( "click", function() {
-  var index = $( ".quadrato1" ).index( this );
-  $( this ).css({
-    borderStyle: "inset",
-    cursor: "auto"
-  });
-  $( "h1" ).text( "Quadrati rossi selezionati:" + (++n));
-});
+for (var k = 0; k < 14; k++) {
+  document.getElementsByClassName('quadrati')[0].innerHTML += '<div class="quadrato"></div>'
+}
+for (var i = 0; i < 48; i++) {
+  document.getElementsByClassName('quadrati')[0].innerHTML += '<div class="quadrato1"></div>'
+}
 
-$(".quadrato1").click(function(){
-  $(this).css("background-color", "red");
-});
 
 var k = 0;
-$(".quadrato").one( "click", function() {
-  var index = $( ".quadrato" ).index( this );
-  $( this ).css({
-    borderStyle: "inset",
-    cursor: "auto"
+
+$(document).ready(function() {
+  var n = 0;
+  $(".quadrato").one( "click", function() {
+    var index = $( ".quadrato" ).index( this );
+    $( this ).css({
+      borderStyle: "inset",
+      cursor: "auto"
+    });
+    $( "h1" ).text( "Quadrati rossi selezionati:" + (++k));
   });
-  $( "h2" ).text( "Quadrati verdi selezionati:" + (++k));
 });
 
-$(".quadrato").click(function(){
-  $(this).css("background-color", "green");
-});
+  $(".quadrato1").click(function(){
+    $(this).css("background-color", "red");
+  });
+
+  var i = 0;
+  $(".quadrato1").one( "click", function() {
+    var index = $( ".quadrato" ).index( this );
+    $( this ).css({
+      borderStyle: "inset",
+      cursor: "auto"
+    });
+    $( "h2" ).text( "Quadrati verdi selezionati:" + (++i));
+  });
+
+  $(".quadrato").click(function(){
+    $(this).css("background-color", "red");
+  });
+
+  $(".quadrato1").click(function(){
+    $(this).css("background-color", "green");
+  });
